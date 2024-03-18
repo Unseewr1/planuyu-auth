bd49d1a3173545d8b725ac7bd232270fa18ba227
Mykytchuk Vladyslav <vladislav.mykytchuk@extern.a1.at>
2024-03-18 15:14:06 +0200

    fixed registration password encoding

--- src/main/java/com/unseewr1/planuyu/dom/User.java
+++ src/main/java/com/unseewr1/planuyu/dom/User.java
@@ -1,10 +1,12 @@
 package com.unseewr1.planuyu.dom;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
+@Setter
 @Document(collection = "users")
 public class User {
 
--- src/main/java/com/unseewr1/planuyu/service/UserService.java
+++ src/main/java/com/unseewr1/planuyu/service/UserService.java
@@ -1,10 +1,10 @@
 package com.unseewr1.planuyu.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unseewr1.planuyu.dom.User;
 import com.unseewr1.planuyu.dto.UserDTO;
 import com.unseewr1.planuyu.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,8 +13,8 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
 
     public User createUser(UserDTO userDTO) {
@@ -24,7 +24,13 @@ public class UserService {
     }
 
     public User toUser(UserDTO userDTO) {
-        return objectMapper.convertValue(userDTO, User.class);
+        User user = new User();
+
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+
+        return user;
     }
 
     public UserDTO toUserDTO(User user) {
